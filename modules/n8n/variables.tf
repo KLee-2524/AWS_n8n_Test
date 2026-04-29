094b098b05f2b2cbe02c37653d2b50e72e94f5ba
KLee-2524 <klee.337x@gmail.com>
2026-04-28 22:45:20 -0700

    n8n setup script

--- modules/n8n/variables.tf
+++ modules/n8n/variables.tf
@@ -30,25 +30,25 @@ variable "n8n_setup_script" {
     default     = <<-EOT
     #!/bin/bash
     export DEBIAN_FRONTEND=noninteractive
-    mkdir /home/ubuntu/TEST
-    echo "TEST directory created" > /home/ubuntu/TEST/setup_log.txt
+    mkdir /home/ubuntu/n8n
+    echo "n8n directory created" > /home/ubuntu/n8n/setup_log.txt
     apt update -y
-    echo "apt update initiated" > /home/ubuntu/TEST/setup_log.txt
+    echo "apt update initiated" > /home/ubuntu/n8n/setup_log.txt
 
     sudo apt-get update -y
     sudo apt-get full-upgrade -y
-    echo "apt-get udpate & upgrade initiated" >> /home/ubuntu/TEST/setup_log.txt
+    echo "apt-get udpate & upgrade initiated" >> /home/ubuntu/n8n/setup_log.txt
 
     sudo curl -fsSL https://get.docker.com | bash -s docker
-    echo "installing docker" >> /home/ubuntu/TEST/setup_log.txt
+    echo "installing docker" >> /home/ubuntu/n8n/setup_log.txt
 
     sudo service docker start
-    echo "starting docker" >> /home/ubuntu/TEST/setup_log.txt
+    echo "starting docker" >> /home/ubuntu/n8n/setup_log.txt
 
     sudo docker volume create n8n_data
-    echo "created n8n_data docker volume" >> /home/ubuntu/TEST/setup_log.txt
+    echo "created n8n_data docker volume" >> /home/ubuntu/n8n/setup_log.txt
 
     sudo docker run -d --name n8n -e N8N_SECURE_COOKIE=false -p 5678:5678 -v n8n_data:/home/ubuntu docker.n8n.io/n8nio/n8n
-    echo "configured n8n for access via web browser" >> /home/ubuntu/TEST/setup_log.txt
+    echo "configured n8n for access via web browser" >> /home/ubuntu/n8n/setup_log.txt
     EOT
 }
